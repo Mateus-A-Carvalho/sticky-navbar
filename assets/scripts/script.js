c14a138d0a01fc48399f33fe1cda5593b6798c96
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-10-29 16:30:27 -0300

    Starting to work in functionalities and trying to solve with scrollHeight

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+const navbarEl = document.querySelector('#navbar');
+const navbarHeight = navbarEl.scrollHeight;
+const sectionEl = document.querySelector('.lorem');
+const sectionHeight = sectionEl.scrollHeight;
+
+document.addEventListener("scroll", () => {
+    if(navbarHeight >= sectionHeight) {
+      navbarEl.style.setProperty("--navbar-dark-bg", "--navbar-light-bg");
+    }
+});
+
+// Note: The scrollHeight of my navbar will be always the same because it's has position:fixed;
