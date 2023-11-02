4e973e82555921fe7b3b7dcfc97ba0e412f72176
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-02 20:26:38 -0300

    Finished project. The goal of this project was reached and I'm finishing here but I must return to fix a bug in the logic that could works wrong in differente zooms in browser.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,23 @@
 const navbarEl = document.querySelector('#navbar');
 const navbarHeight = navbarEl.scrollHeight;
-const logoEl = document.querySelector(".logo")
-const sectionEl = document.querySelector('.lorem');
-const sectionHeight = sectionEl.scrollHeight;
+const logoEl = document.querySelector("#logo-img")
+const sectionEl = document.querySelector("#lorem-section").scrollHeight;
 const bgWrapperElHeight = document.querySelector(".bg-wrapper").scrollHeight;
 
+console.log(sectionEl);
 
 document.addEventListener("scroll", () => {
   let pointToChange = bgWrapperElHeight - navbarHeight - 5;
-  console.log(scrollY)
-  console.log(pointToChange);
-
+  console.log(pointToChange, scrollY);
+  
   if(scrollY >= pointToChange) {
-    // console.log(`Point Reached!`)
-    navbarEl.style.setProperty("--navbar-light-bg", "rgba(25, 25, 25, 1)");
-    navbarEl.style.setProperty("--navbar-fontLight-color", "rgba(230, 230, 230, .8)");
-  } else if (scrollY <= pointToChange) {
-    navbarEl.style.setProperty("rgba(25, 25, 25, 1)", "--navbar-light-bg");
-    navbarEl.style.setProperty("rgba(230, 230, 230, .8)", "--navbar-fontLight-color");
+    navbarEl.style.backgroundColor = "rgba(25, 25, 25, 1)";
+    navbarEl.style.color = "rgba(230, 230, 230, .8)";
+    logoEl.style.border = ".5rem solid rgba(230, 230, 230, 1)";
+  } else {
+    navbarEl.style.backgroundColor = "rgba(230, 230, 230, 1)";
+    navbarEl.style.color = "rgba(23, 29, 54, .8)";
+    logoEl.style.border = ".5rem solid rgba(168, 166, 166, 0.8)";
   }
 });
 
