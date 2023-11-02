fb59c2c08f5c91478122263400e511ce0cce7654
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-02 06:17:05 -0300

    Just missing make a logic that bring back the properties when I get out of element height's. I used two properties to make this projec; scrollY and scrollHeight. scrollY to get height of the scrollbar and scrollHeight to get the height of specific element, with his invisible parts. Used a variable to calculate the height of element minus height of my navbar and his border. Also used the .addEventListener to caught 'scroll' event.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ const navbarHeight = navbarEl.scrollHeight;
 const logoEl = document.querySelector(".logo")
 const sectionEl = document.querySelector('.lorem');
 const sectionHeight = sectionEl.scrollHeight;
-const bgWrapperElHeight = document.querySelector(".bg-wrapper").scrollHeight
+const bgWrapperElHeight = document.querySelector(".bg-wrapper").scrollHeight;
 
 
 document.addEventListener("scroll", () => {
@@ -12,12 +12,13 @@ document.addEventListener("scroll", () => {
   console.log(pointToChange);
 
   if(scrollY >= pointToChange) {
-    console.log(`Point Reached!`)
-    navbarEl.style.setProperty("--navbar-light-bg", "--navbar-dark-bg");
+    // console.log(`Point Reached!`)
+    navbarEl.style.setProperty("--navbar-light-bg", "rgba(25, 25, 25, 1)");
+    navbarEl.style.setProperty("--navbar-fontLight-color", "rgba(230, 230, 230, .8)");
+  } else if (scrollY <= pointToChange) {
+    navbarEl.style.setProperty("rgba(25, 25, 25, 1)", "--navbar-light-bg");
+    navbarEl.style.setProperty("rgba(230, 230, 230, .8)", "--navbar-fontLight-color");
   }
 });
 
-// let bgNavBar = navbarEl.style.setProperty("--navbar-light-bg", "--navbar-dark-bg");
-//     let colorNavBar = navbarEl.style.setProperty("--navbar-fontLight-color", "--navbar-fontDark-color");
-//     let borderLogo = logoEl.style.setProperty("--logo-LightBorder", "--logo-DarkBorder")
 
