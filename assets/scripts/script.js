0d6942289a4670eab3f80d8536d10b707221985e
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-01 22:22:31 -0300

    I get the value of scroll and just miss adjust the logic and the CSS variables to do the correct change.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,23 @@
 const navbarEl = document.querySelector('#navbar');
 const navbarHeight = navbarEl.scrollHeight;
+const logoEl = document.querySelector(".logo")
 const sectionEl = document.querySelector('.lorem');
 const sectionHeight = sectionEl.scrollHeight;
+const bgWrapperElHeight = document.querySelector(".bg-wrapper").scrollHeight
+
 
 document.addEventListener("scroll", () => {
-    if(navbarHeight >= sectionHeight) {
-      navbarEl.style.setProperty("--navbar-dark-bg", "--navbar-light-bg");
-    }
+  let pointToChange = bgWrapperElHeight - navbarHeight - 5;
+  console.log(scrollY)
+  console.log(pointToChange);
+
+  if(scrollY >= pointToChange) {
+    console.log(`Point Reached!`)
+    navbarEl.style.setProperty("--navbar-light-bg", "--navbar-dark-bg");
+  }
 });
 
-// Note: The scrollHeight of my navbar will be always the same because it's has position:fixed;
-// "how to get which height element is in scrollbar in Javascript?"
+// let bgNavBar = navbarEl.style.setProperty("--navbar-light-bg", "--navbar-dark-bg");
+//     let colorNavBar = navbarEl.style.setProperty("--navbar-fontLight-color", "--navbar-fontDark-color");
+//     let borderLogo = logoEl.style.setProperty("--logo-LightBorder", "--logo-DarkBorder")
+
